6fd8f366798aca4662f9bfb4dfdd042cb04e1d38
engaymansidahmed-afk <eng.aymansidahmed@gmail.com>
2026-06-28 13:32:13 +0300

    Update EmployeeService.ts

--- src/ services/EmployeeService.ts
+++ src/ services/EmployeeService.ts
@@ -1,1 +1,62 @@
+import { EmployeeRepository } from "../repositories/EmployeeRepository";
 
+export class EmployeeService {
+
+    static async getAll() {
+
+        return await EmployeeRepository.findAll();
+
+    }
+
+    static async getById(id: string) {
+
+        const employee = await EmployeeRepository.findById(id);
+
+        if (!employee) {
+
+            throw new Error("Employee not found.");
+
+        }
+
+        return employee;
+
+    }
+
+    static async create(data: any) {
+
+        return await EmployeeRepository.create(data);
+
+    }
+
+    static async update(
+        id: string,
+        data: any
+    ) {
+
+        const employee = await EmployeeRepository.findById(id);
+
+        if (!employee) {
+
+            throw new Error("Employee not found.");
+
+        }
+
+        return await EmployeeRepository.update(id, data);
+
+    }
+
+    static async delete(id: string) {
+
+        const employee = await EmployeeRepository.findById(id);
+
+        if (!employee) {
+
+            throw new Error("Employee not found.");
+
+        }
+
+        return await EmployeeRepository.delete(id);
+
+    }
+
+}
